fe0b36d8157f64426efabdb30f48d51ac4e5da88
shuuuuuny <shuuuuuny@gmail.com>
2021-01-03 18:34:22 +0900

    fix

--- ruby3-study/runner/fizz_buzz_runner.rb
+++ ruby3-study/runner/fizz_buzz_runner.rb
@@ -2,7 +2,7 @@ require 'date'
 require 'retryable'
 require_relative '../lib/fizz_buzz'
 
-Retryable.retryable(tries: '3') do
+Retryable.retryable(tries: 3) do
   results = FizzBuzz.run(Date.today.day)
   puts results
 end
